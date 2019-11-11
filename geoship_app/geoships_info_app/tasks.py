54d6e3241e00c30c07c20feb82f52e6e97e32021
Khanze99 <khamidov_anvar@mail.ru>
2019-11-11 21:06:28 +0300

    Update readme and update celery task

--- geoship_app/geoships_info_app/tasks.py
+++ geoship_app/geoships_info_app/tasks.py
@@ -5,6 +5,6 @@ from celery.schedules import crontab
 from .upload_to_db import check_files
 
 
-@periodic_task(run_every=crontab(minute='*/1'), name="push_date_to_parse")
+@periodic_task(run_every=crontab(minute=0, hour=0), name="push_date_to_parse")
 def push_date_to_parse():
     check_files()
