a75c3f234a74823bdc46a4925a49c61eabf49f28
Khanze99 <khamidov_anvar@mail.ru>
2019-11-11 20:44:39 +0300

    Add inline for vessel with history

--- geoship_app/geoships_info_app/admin.py
+++ geoship_app/geoships_info_app/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Vessel
+from .models import Vessel, History
 
-admin.site.register(Vessel)
+
+class HistoryInLine(admin.TabularInline):
+    model = History
+
+
+class VesselAdm(admin.ModelAdmin):
+    inlines = [
+        HistoryInLine,
+    ]
+
+
+admin.site.register(Vessel, VesselAdm)
+admin.site.register(History)
